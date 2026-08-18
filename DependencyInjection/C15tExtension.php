eb68a75e5366243339ec4e19a5065b71b264dff2
Alan Hartless <63312+alanhartless@users.noreply.github.com>
2026-08-17 23:32:10 -0500

    Fix namespace

--- DependencyInjection/C15tExtension.php
+++ DependencyInjection/C15tExtension.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace MauticPlugin\GrapesJsBuilderBundle\DependencyInjection;
+namespace MauticPlugin\C15tBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
